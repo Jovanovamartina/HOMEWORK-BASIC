d0d185ed75238c95617463a200f3b1f5a5b892e6
Jovanovamartina <98462497+Jovanovamartina@users.noreply.github.com>
2022-03-15 23:29:33 +0100

    homework

---
+++ HOMEWORK_02/exercise1/exercise1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercise one
+
+            double a = 5.5D;
+            double b = 2.7D;
+            double c = a / b;
+            Console.WriteLine("the result is " + c);
+
+            int numberOne = 10;
+            int numberTwo = 5;
+            int result = numberOne / numberTwo;
+            Console.WriteLine("second result is " + result);
+
+            //exercise two
+
+            int n = 102;
+            int m = 5;
+            int TheResult = n / m;
+            Console.WriteLine("you can send " + TheResult);
+        }
+    }
+}
