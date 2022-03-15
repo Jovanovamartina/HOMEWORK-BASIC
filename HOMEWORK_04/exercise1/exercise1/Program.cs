f7a44c554bdb33505e21444812e9a99729835257
Jovanovamartina <98462497+Jovanovamartina@users.noreply.github.com>
2022-03-15 23:42:33 +0100

    homework

---
+++ HOMEWORK_04/exercise1/exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int i, even = 0;
+			int[] arr = new int[6];
+
+			Console.Write("Enter six numbers:");
+
+			for (i = 0; i < arr.Length; i++)
+			{
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			for (i = 0; i < 6; i++)
+			{
+				if (arr[i] % 2 == 0)
+				{
+					even = even + arr[i];
+				}
+
+			}
+			Console.WriteLine("Sum of even list is:" + even);
+
+		}
+	}
+}
---
+++ HOMEWORK_04/exercise1/exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] G1 = new string[5] { "martina", "sofia", "ana", "melani", "sara" };
+            string[] G2 = new string[5] { "david", "damjan", "marko", "kostadin", "vladimir" };
+
+            Console.WriteLine("enter student group between 1 and 2");
+            Console.ReadLine();
+            int FirstNumber = 1;
+            int SecondNumber = 2;
+        }
+    }
+}
