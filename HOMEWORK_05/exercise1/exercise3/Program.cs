953c578dd36716abc7f00c9c9eee490df584e460
Jovanovamartina <98462497+Jovanovamartina@users.noreply.github.com>
2022-03-21 01:32:19 +0100

    Update Program.cs

--- HOMEWORK_05/exercise1/exercise3/Program.cs
+++ HOMEWORK_05/exercise1/exercise3/Program.cs
@@ -1,24 +1,62 @@
-﻿using System;
- 
-namespace exercise3
-{
-    internal class Program
-    {
+using System;
 
-       
+namespace ageCalculator
+{
+    class Program
+    {
         static void Main(string[] args)
         {
-
-            static int CalculateAge(DateTime birthDate, DateTime now)
+            Console.WriteLine("Write your Birthday in the following format dd/mm/yy:");
+            string dateOfBirth = Console.ReadLine();
+            if (dateOfBirth.Length != 8 || int.Parse(dateOfBirth.Substring(0, 2)) > 31 || int.Parse(dateOfBirth.Substring(3, 2)) > 12)
             {
-                int age = now.Year - birthDate.Year;
-
-                if (now.Month < birthDate.Month || (now.Month == birthDate.Month && now.Day < birthDate.Day))
-                    age--;
-
-                return age;
+                Console.WriteLine("Wrong format. Please insert your date of birth like this: " +
+                    "\n15.05.95 or 08/08/08 or 03-11-87 \nWith separating sign and  zeros no matter the number.");
             }
+            else
+                CalculateAge(dateOfBirth);
+                Console.WriteLine(CalculateAge(dateOfBirth));
+            Console.ReadLine();
+
+
 
         }
+
+        static int CalculateAge(string birthDate)
+        {
+            int age = 0;
+            int birthYear;
+            int dateOfBirth = int.Parse(birthDate.Substring(0, 2));
+            int monthOfBirth = int.Parse(birthDate.Substring(3, 2));
+            int yearOfBirth = int.Parse(birthDate.Substring(6, 2));
+            int dateNow = int.Parse(DateTime.Now.Day.ToString());
+            int monthNow = int.Parse(DateTime.Now.Month.ToString());
+            int yearNow = int.Parse(DateTime.Now.Year.ToString());
+            
+            if (yearOfBirth < 22)
+            {
+                birthYear = (2000 + yearOfBirth);
+            }
+            else birthYear = (1900 + yearOfBirth);
+            
+            if (dateOfBirth > monthNow)
+            {
+                age = yearNow - birthYear;
+            }
+            else if (monthOfBirth == monthNow && birthYear > dateNow)
+            {
+                age = yearNow - birthYear;
+            }
+            else if (monthOfBirth < monthNow)
+            {
+                age = yearNow - birthYear;
+            }
+            else if (monthOfBirth == monthNow && dateOfBirth <= dateNow)
+            {
+                age = yearNow - birthYear;
+            }
+            return age;
+        }
+
     }
 }
