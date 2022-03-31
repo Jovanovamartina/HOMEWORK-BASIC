e763117002d82516232d5132d8a499fb395cbc18
Jovanovamartina <98462497+Jovanovamartina@users.noreply.github.com>
2022-03-31 12:31:44 +0200

    homework

---
+++ HOMEWORK_08/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue <int> numbers = new Queue <int> ();
+            while (true)
+            {
+                Console.Write("Enter a number please : ");
+                if (!int.TryParse(Console.ReadLine(), out int num))
+                {
+                    Console.WriteLine("Input is not a number , please enter a number");
+                    Console.ReadLine();
+                    continue;
+                }
+                numbers.Enqueue(num);
+                Console.WriteLine("Would you like to enter a different number (yes/no) ? ");
+                string input = Console.ReadLine();
+                if (input.ToLower() != "yes")
+                {
+                    break;
+                }
+                continue;
+            }
+            while (numbers.TryDequeue(out int num))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
